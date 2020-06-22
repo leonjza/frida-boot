fc2c8c8306914f6f60d7101e9770a3b19ac522f7
hexpwn <58829864+hexpwn@users.noreply.github.com>
2020-06-22 20:20:43 +0000

    Return to original fake_sleep.c presented in the video
    The current version of the file is a copy of fake_sleep.c.2 and different from what is presented in the class.

--- software/fake_sleep.c
+++ software/fake_sleep.c
@@ -7,13 +7,7 @@
 //  unsigned int sleep(unsigned int seconds);
 unsigned int sleep(unsigned int seconds) {
 
-    // you've never slept the well in your life!
+    // you've never slept this well in your life!
     printf("[-] sleep goes brrr\n");
-
-    seconds = 1;
-
-    unsigned int (*original_sleep)(unsigned int);
-    original_sleep = dlsym(RTLD_NEXT, "sleep");
-
-    return (original_sleep)(seconds);
+    return 0;
 }
